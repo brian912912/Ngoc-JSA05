81a3039eed60d9976aa9a79cc7e4156015c10c32
brian912912 <hovanngoc912@gmail.com>
2023-08-13 12:02:50 +0700

    Buổi2

---
+++ jsa05.js
@@ -0,0 +1,12 @@
+
+//let so =[0 ,1 ,2]
+//console.log(so[0]);
+//console.log(so[1]);
+//console.log(so[2]);
+let chu =["Hiếu ","Ngọc" ,"Tài"]
+//console.log(chu[0]);
+//console.log(chu[1]);
+//console.log(chu[2]);
+let i = 0;
+do{console.log(i +":"+chu[i] );i++;}
+while(i<3)
