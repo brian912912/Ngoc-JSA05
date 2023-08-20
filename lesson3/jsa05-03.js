59804d95655cf036ac62d78efbb0dc4d9f113624
brian912912 <hovanngoc912@gmail.com>
2023-08-20 12:02:19 +0700

    buoi3ne

---
+++ lesson3/jsa05-03.js
@@ -0,0 +1,12 @@
+let arr = ["Toán", "Ngữ Văn", "Tiếng Anh "]
+for ( let value of arr )
+    console.log(value)
+
+let obj = {
+    'name': "Nguyễn Văn A",
+    'sex' : "0 xác định",
+    'crime' : "Vi phạm hành chính"
+}
+
+for(let i in obj)
+console.log(obj[i])
