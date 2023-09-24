856fac1d377eeb645aa25f1609c5b7353675d5a4
brian912912 <hovanngoc912@gmail.com>
2023-09-24 12:01:42 +0700

    b7

---
+++ lesson6/jsa05-06.js
@@ -0,0 +1,40 @@
+// get: trả về 1 resouse / danh sách resource
+// post: hỗ trợ tạo mới 1 resource
+// put: hỗ trợ cập nhật thông tin cho resource
+// delete: xóa resource
+//fetch : gửi và nhận resouce
+
+// function f1 (){
+//    return  new Promise(resolve => {
+//       setTimeout(()=>{
+//       resolve ('take2sec')},2000)
+
+//    });
+// }
+
+// async function f2 (){
+//    console.log ("Bắt đầu")
+//    const cho = await f1();
+//    console.log(cho)
+
+// }
+// f2();
+
+
+[
+   {
+      "name" : "Ngọc",
+      "age"  :"14"
+   },
+   {
+      "name" : "Huy ",
+      "age" : "14"
+   },
+   {
+      "name" : "Tài",
+      "age" : "18"
+   }
+
+]
+
+
